0fdebc1b178b69cf19c96977320ba0023d98fee7
GrapheneOS <contact@grapheneos.org>
2021-07-29 03:13:00 +0300

    gmscompat: support for Dynamite modules
    Authored by Danny Lin <danny@kdrag0n.dev> and Dmitry Muhomor
    <muhomor.dmitry@gmail.com> for inclusion in GrapheneOS.
    
    Commit history:
    
    June 2024 - August 2024:
    https://github.com/GrapheneOS/platform_libcore/commits/2024083100
    
    Before June 2022:
    https://github.com/GrapheneOS/platform_libcore/commits/SP2A.220505.002.2022053100
    
    Before Android 12:
    https://github.com/GrapheneOS/platform_libcore/commits/RQ3A.211001.001.2021100606

--- ojluni/src/main/java/java/util/zip/ZipFile.java
+++ ojluni/src/main/java/java/util/zip/ZipFile.java
@@ -1522,6 +1522,7 @@ public class ZipFile implements ZipConstants, Closeable {
             public Key(File file, BasicFileAttributes attrs, ZipCoder zc,
                     boolean isZipFilePathValidatorEnabled) {
                 this.attrs = attrs;
+
                 this.file = file;
                 this.utf8 = zc.isUTF8();
                 this.isZipFilePathValidatorEnabled = isZipFilePathValidatorEnabled;
@@ -1639,6 +1640,7 @@ public class ZipFile implements ZipConstants, Closeable {
             } else {
                 // Android-changed: open with O_CLOEXEC flag set.
                 // this.zfile = new RandomAccessFile(key.file, "r");
+                // todo
                 this.zfile = new RandomAccessFile(key.file, "r", /* setCloExecFlag= */ true);
             }
             try {
