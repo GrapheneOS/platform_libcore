f4ebf0438fc78fa554ccbbf13fe7cddc40659305
Daniel Micay <danielmicay@gmail.com>
2022-08-16 16:50:48 -0400

    constify JNINativeMethod tables
    Co-authored-by: r3g_5z <june@girlboss.ceo>

--- ojluni/src/main/native/ZipFile.c
+++ ojluni/src/main/native/ZipFile.c
@@ -393,7 +393,7 @@ JarFile_getMetaInfEntryNames(JNIEnv *env, jobject obj)
     return result;
 }
 
-static JNINativeMethod gMethods[] = {
+static const JNINativeMethod gMethods[] = {
   NATIVE_METHOD(ZipFile, getFileDescriptor, "(J)I"),
   NATIVE_METHOD(ZipFile, getEntry, "(J[BZ)J"),
   NATIVE_METHOD(ZipFile, freeEntry, "(JJ)V"),
